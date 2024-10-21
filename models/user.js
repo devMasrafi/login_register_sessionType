3bb3821dbb09c6adc0d64376d15689b23956d41a
Md. Masrafi Mondol <146985678+devMasrafi@users.noreply.github.com>
2024-10-21 10:48:08 +0600

    model and initial setup

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    userName:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    userEmail:{
+        type: String,
+        unique: true,
+        required: true,
+    },
+    userPasoword:{
+        type: String,
+        required: true,
+    },
+    useProfileImg:{
+        type: String,
+    }
+})
+
+module.exports = mongoose.model("User", userSchema)
